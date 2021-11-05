b4a496be57226bde35ec19711dd63312aab6c634
charlotte49 <charlotte.menu@gmail.com>
2021-11-05 20:18:49 +0100

    troisième commit

--- src/TPfonction.java
+++ src/TPfonction.java
@@ -1,20 +1,20 @@
 public class TPfonction {
-    static int[] direction(String[] dir){
-        int tab[] = new int[2];
-        for (int i = 0; i<dir.length; i++){
-            if (dir[i] == "haut"){
-                tab[0]= +1;
+        static int[] direction(String[] dir){
+            int tab[] = new int[2];
+            for (int i = 0; i < dir.length; i++) {
+                if (dir[i] == "haut") {
+                    tab[0] = +1;
+                }
+                if (dir[i] == "bas") {
+                    tab[0] = -1;
+                }
+                if (dir[i] == "gauche") {
+                    tab[1] = -1;
+                }
+                if (dir[i] == "droite") {
+                    tab[1] = +1;
+                }
             }
-            if (dir[i] == "bas"){
-                tab[0] = -1;
-            }
-            if (dir[i] == "gauche"){
-                tab[1] = -1;
-            }
-            if (dir[i] == "droite"){
-                tab[1]=+1;
-            }
+            return tab;
         }
-        return tab;
-    }
 }
